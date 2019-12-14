421b593180478c96fc2dd5c0ca5eba4d28bf9ef4
AmarArjunGunjal <amargunjal28@gmail.com>
2019-12-14 10:21:01 +0530

    Added :Functionality for 2 player and display winner player

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -3,16 +3,15 @@
 echo "Welcome to Snake & Ladder "
 
 position=0
-endPosition=100
-diceCount=0
+player1Position=0
+player2Position=0
+winningPosition=100
 dice=0
 
 function getPosition()
 {
-while [ $position -lt $endPosition ]
-do
+	position=$1
 	diceRandom=$(($RANDOM%6 +1 ))
-	((diceCount++))
 	option=$(($RANDOM%3))
 
 	case $option in
@@ -31,19 +30,53 @@ do
 	then
 		position=0
 	fi
-	if [ $position -gt $endPosition ]
+
+	if [ $position -gt $winningPosition ]
 	then
-		position=$(($postition-$diceRandom))
+		position=$(($position-$diceRandom))
 	fi
 
-	positionDictionary[$dice]="$position"
-	dice=$(( $dice + 1 ))
-	echo "Position : " $position
-
-done
-
+	echo $position
 }
-getPosition
-echo "Number of time dice rolled : "$diceCount
 
+flag=1
 
+function getWinner()
+{
+while [[ $player1Position -lt $winningPosition && $player2Position -lt $winningPosition ]]
+do
+	dice=$(( $dice + 1 ))
+	if [ $flag -eq 1 ]
+	then
+		player1Position=$(getPosition $player1Position)
+		echo "player1 position : "$player1Position
+		flag=0
+				if [ $player1Position -eq $winningPosition ]
+				then
+					echo "$player1 win !!!"
+					exit
+				fi
+	fi
+
+	if [ $flag -eq 0 ]
+	then
+			player2Position=$(getPosition $player2Position)
+			echo "player2 position : "$player2Position
+			flag=1
+				if [ $player2Position -eq $winningPosition ]
+					then
+							echo "$player2 win !!!!"
+					break;
+				fi
+	fi
+done
+}
+
+function main()
+{
+echo "Total Dice Rolled : "$dice
+read -p "Enter 1st Player Name : " player1
+read -p "Enter 2nd Player Name : " player2
+getWinner
+}
+main
